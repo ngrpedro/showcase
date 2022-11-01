bbac833e79034e056bf847bc85b0ffbc0d4f5b74
Pedro Nogueira <ngrpedro22@gmail.com>
2022-11-01 18:37:27 -0300

    gymone

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import GymOne from "./pages/GymOne";
 import Home from "./pages/Home";
 import Organizze from "./pages/Organizze";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/organizze" element={<Organizze />} />
-      <Route path="/gymone" element={<GymOne />} />
-    </Routes>
+    <div>
+      <nav className="py-8  px-6 md:px-16 flex items-center justify-between">
+        <Link to="/">
+          <h1>Home</h1>
+        </Link>
+        <div className="flex gap-10">
+          <Link to="/organizze">Organizze</Link>
+          <Link to="/gymone">GynOne</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/organizze" element={<Organizze />} />
+        <Route path="/gymone" element={<GymOne />} />
+      </Routes>
+    </div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="p-10">
-      <h1>Home</h1>
-      <div className="flex gap-10 mt-10">
-        <Link to="/organizze">Organizze</Link>
-        <Link to="/gymone">GynOne</Link>
-      </div>
+    <div>
+      <div className="p-10">Hey there ::)</div>
     </div>
   );
 };
