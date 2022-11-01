d7ba76cece2fa58991df1cdf24d28bdbd0947b58
Pedro Nogueira <ngrpedro22@gmail.com>
2022-11-01 18:46:17 -0300

    gymone

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="p-10">Hey there ::)</div>
+      <div className="py-8 px-6 md:px-16 space-y-8">
+        <h1 className="text-2xl ">Hey there :)</h1>
+        <p className="text-lg">
+          Navegue pelas abas do menu para ver as landings pages disponíveis!
+        </p>
+      </div>
     </div>
   );
 };
