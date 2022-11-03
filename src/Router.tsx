0221dc5bdb645b43f28a55a88c09b9dab5fc6c73
Pedro Nogueira <ngrpedro22@gmail.com>
2022-11-03 16:35:07 -0300

    home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,5 @@
-import { ArrowSquareOut } from "phosphor-react";
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import GymOne from "./pages/GymOne";
 import Home from "./pages/Home";
 import Organizze from "./pages/Organizze";
@@ -8,7 +7,7 @@ import Organizze from "./pages/Organizze";
 const Router = () => {
   return (
     <div>
-      <nav
+      {/* <nav
         className="py-8 px-6 md:px-16 flex items-center justify-between
       bg-white"
       >
@@ -16,7 +15,7 @@ const Router = () => {
           <h1>Home</h1>
         </Link>
         <div className="flex gap-10">
-          <Link to="/organizze" target={"_blank"}>Organizze</Link>
+          <Link to="/organizze">Organizze</Link>
           <Link to="/gymone">GynOne</Link>
           <a
             href="https://greenlp-react.vercel.app/"
@@ -26,7 +25,7 @@ const Router = () => {
             GreenOne <ArrowSquareOut size={18} />
           </a>
         </div>
-      </nav>
+      </nav> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/organizze" element={<Organizze />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,59 @@
 import React from "react";
 
+const projects = [1, 2, , 3, 4];
+
 const Home = () => {
   return (
     <div className="w-full h-screen bg-white">
-      <div className="py-8 px-6 md:px-16 space-y-8">
-        <h1 className="text-2xl ">Hey there :)</h1>
-        <p className="text-lg">
-          Pela barra de navegação, veja as Landing Pages disponíveis!
-        </p>
+      <div className="">
+        <div className="grid grid-cols-1 md:grid-cols-2 py-16 px-6 md:px-16 gap-16">
+          <div className="flex items-start justify-start gap-16">
+            <div
+              className="rounded-2xl bg-gray-200 w-[10rem] h-[10rem]
+              origin-center rotate-45"
+            ></div>
+            <div className="space-y-8 max-w-xl">
+              <h1 className="text-4xl">
+                Hey there :)<span className="block mt-2">I'm Pedro</span>
+              </h1>
+              <div className="space-y-4">
+                <p className="text-sm">
+                  Aute veniam eu labore consectetur dolor deserunt nostrud.
+                  Occaecat irure mollit laborum mollit sint. Aliqua non dolor
+                  magna veniam magna aute commodo elit occaecat.
+                </p>
+                <p className="text-sm">
+                  Aute veniam eu labore consectetur dolor deserunt nostrud.
+                  Occaecat irure mollit laborum mollit sint. Aliqua non dolor
+                  magna veniam magna aute commodo elit occaecat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-8 px-6 md:px-16">
+          <p className="text-2xl font-bold mb-10">
+            Aqui estão alguns projetos meus::
+          </p>
+          <div className="grid grid-cols-2 items-start justify-center gap-5 flex-wrap">
+            {projects.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className=" bg-gray-200 p-4 rounded-lg space-y-4"
+                >
+                  <h1>Project Name</h1>
+                  <p>
+                    Ipsum do nulla veniam nulla irure sint esse ad eiusmod
+                    eiusmod laborum esse. Sit sit officia amet voluptate labore
+                    eu aliquip ex dolor laboris tempor nulla non laborum.
+                    Nostrud ad laborum
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
